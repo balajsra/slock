b551c3de207609a2845bb43978754096b982f691
Sravan Balaji <balajsra@umich.edu>
2020-12-11 11:19:18 -0500

    Colors & Font Changes
    - Change colors
    - Disable fail on clear
    - Change message font name

--- config.def.h
+++ config.def.h
@@ -3,20 +3,20 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "red",         /* CapsLock on */
+	[INIT]   = "black",  /* after initialization */
+	[INPUT]  = "purple", /* during input */
+	[FAILED] = "red",    /* wrong password */
+	[CAPS]   = "cyan",   /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "Display Locked with Slock";
 
 /* text color */
-static const char * text_color = "#ffffff";
+static const char * text_color = "white";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "lucidasans-bold-24";
